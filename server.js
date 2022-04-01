d2845b0c254b8dcb8cb3fc226cdb9ae011fb515d
Solarc117 <jonathan.carlos.bernal@gmail.com>
2022-03-31 19:41:12 -0600

    🐶 pug view engine, render index

--- server.js
+++ server.js
@@ -1,21 +1,30 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+'use strict'
+require('dotenv').config()
+const express = require('express')
+const myDB = require('./connection')
+const fccTesting = require('./freeCodeCamp/fcctesting.js')
 
-const app = express();
+const app = express()
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug')
+
+fccTesting(app) // For FCC testing purposes.
+app.use('/public', express.static(process.cwd() + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
-});
+  // res.render('pug/index.pug') // ✅
+  // res.render('./pug/index.pug') // ✅
+  // res.render('pug/index.pug') // ✅
+  // res.render('./pug/index') // ✅
+  // res.render('/pug/index') // ❌
+  // res.render('pug/index') // ✅
+  // res.render('./pug/') // ✅
+  // res.render('/pug/') // ❌
+  // res.render('pug/') // ✅
+  res.render('pug') // ✅
+})
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
-});
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log('Listening on port ' + PORT))
