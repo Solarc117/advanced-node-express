45062ee049a65030ef5b574d850da142af52da97
Solarc117 <jonathan.carlos.bernal@gmail.com>
2022-04-05 16:34:18 -0600

    feat: :ambulance: ignore .env, install and init passport and express-session in server.js
    renamed sample.env to .env so it is ignored in .gitignore, set up express-sesion and passport

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict'
 require('dotenv').config()
-const express = require('express')
-const myDB = require('./connection')
-const fccTesting = require('./freeCodeCamp/fcctesting.js')
+const express = require('express'),
+  // myDB = require('./connection'),
+  fccTesting = require('./freeCodeCamp/fcctesting.js'),
+  session = require('express-session'),
+  passport = require('passport')
 
 const app = express()
 
@@ -12,6 +14,16 @@ fccTesting(app) // For FCC testing purposes.
 app.use('/public', express.static(process.cwd() + '/public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.route('/').get((req, res) => {
   // res.render('pug/index.pug') // ✅
