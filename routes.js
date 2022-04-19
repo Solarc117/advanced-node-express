5645aaf8e246431a540461b11207d241c910221c
Solarc117 <jonathan.carlos.bernal@gmail.com>
2022-04-19 17:34:08 -0600

    fix: :bug: fixed hash comparison bug while logging in, added logs to fix GH oauth bug

--- routes.js
+++ routes.js
@@ -1,6 +1,10 @@
 const passport = require('passport'),
   bcrypt = require('bcrypt')
 
+function log() {
+  console.log(...arguments)
+}
+
 module.exports = function (app, myDataBase) {
   app.route('/').get((req, res) =>
     res.render('pug', {
@@ -12,21 +16,30 @@ module.exports = function (app, myDataBase) {
     })
   )
 
-  app
-    .route('/login')
-    .post(
-      passport.authenticate('local', { failureRedirect: '/' }),
-      (req, res) => res.redirect('/profile')
-    )
-
-  app.route('/profile').get(ensureAuthenticated, (req, res) =>
-    res.render(process.cwd() + '/views/pug/profile', {
-      // @ts-ignore
-      username: req.user.username,
-    })
+  app.route('/login').post(
+    passport.authenticate('local', {
+      failureRedirect: '/',
+      failureMessage: true,
+    }),
+    (req, res) => {
+      log(`Redirecting ${req.user.username} to /profile...`)
+      res.redirect('/profile')
+    }
   )
 
+  app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    const { username } = req.user
+    log(`GET /profile`)
+
+    res.render(process.cwd() + '/views/pug/profile', {
+      username,
+    })
+  })
+
   app.route('/logout').get((req, res) => {
+    const { username } = req.user
+    log(`Logging out ${username}...`)
+
     req.logout()
     res.redirect('/')
   })
@@ -35,31 +48,63 @@ module.exports = function (app, myDataBase) {
     (req, res, next) => {
       const { username, password } = req.body,
         hash = bcrypt.hashSync(password, 12)
+      log(`Attempting to register user ${username}...`)
 
       myDataBase.findOne({ username }, (err, existingUser) => {
-        return err
-          ? next(err)
-          : existingUser
-          ? res.redirect('/')
-          : myDataBase.insertOne({ username, password: hash }, (err, newUser) =>
-              // @ts-ignore
-              err ? res.redirect('/') : next(null, newUser.ops[0])
-            )
+        if (err) {
+          console.error(
+            'Could not query user during registration process: ',
+            err
+          )
+          return next(err)
+        }
+
+        if (existingUser) {
+          console.warn(
+            `Could not register - a user with username ${username} already exists`
+          )
+          return res.redirect('/')
+        }
+
+        myDataBase.insertOne({ username, password: hash }, (err, newUser) => {
+          if (err) {
+            console.error(`Could not create new user ${username}: `, err)
+            return res.redirect('/')
+          }
+
+          log(`Successfully registered new user ${username}`)
+          next(null, newUser.ops[0])
+        })
       })
     },
-    passport.authenticate('local', { failureRedirect: '/' }),
-    (req, res, next) => res.redirect('/profile')
+    passport.authenticate('local', {
+      failureRedirect: '/',
+      failureMessage: true,
+    }),
+    (req, res, next) => {
+      const { username } = req.user
+
+      log(`Successfully registered ${username} - redirecting to /profile...`)
+      res.redirect('/profile')
+    }
   )
 
   app.route('/auth/github').get(passport.authenticate('github'))
 
-  app
-    .route('/auth/github/callback')
-    .get(
-      passport.authenticate('github', { failureRedirect: '/' }, (req, res) =>
+  app.route('/auth/github/callback').get(
+    passport.authenticate(
+      'github',
+      { failureRedirect: '/', failureMessage: true },
+      (req, res) => {
+        const { username } = req.user
+        log(
+          `Succesfully authenticated user ${username} with Github - redirecting to /profile...`
+        )
+
         res.redirect('/profile')
-      )
+      }
     )
+  )
 
   app.use((req, res, next) => res.status(404).type('text').send('Not Found'))
 }
